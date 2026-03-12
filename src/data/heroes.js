d9249558a15773e88828c79c6f410a97bb22759b
Adolf Mayer <4dolf.mayer@gmail.com>
2026-03-12 15:52:37 +0100

    Prvni funkcni verze deniku

---
+++ src/data/heroes.js
@@ -0,0 +1,42 @@
+export const heroes = [
+  {
+    id: 'vin-diesel',
+    name: 'Vin Diesel',
+    race: 'Inox',
+    traits: ['Outcast ', 'Resourceful ', 'Strong'],
+    image:
+      'https://raw.githubusercontent.com/any2cards/worldhaven/refs/heads/master/images/art/frosthaven/characters/images/fh-drifter.png',
+    color: 'from-blue-900/20',
+    borderColor: 'group-hover:border-blue-500/50',
+  },
+  {
+    id: 'surimi-jones',
+    name: 'Surimi Jones',
+    race: 'Lurker',
+    traits: ['Armored ', 'Chaotic ', 'Strong'],
+    image:
+      'https://raw.githubusercontent.com/any2cards/worldhaven/refs/heads/master/images/art/frosthaven/characters/images/fh-crashing-tide.png',
+    color: 'from-pink-900/20',
+    borderColor: 'group-hover:border-pink-500/50',
+  },
+  {
+    id: 'blaze-chilli',
+    name: 'Blaze Chilli & Lime',
+    race: 'Savvas',
+    traits: ['Arcane ', 'Chaotic ', 'Intimidating'],
+    image:
+      'https://raw.githubusercontent.com/any2cards/worldhaven/refs/heads/master/images/art/frosthaven/characters/images/fh-pyroclast.png',
+    color: 'from-red-900/20',
+    borderColor: 'group-hover:border-red-500/50',
+  },
+  {
+    id: 'slim-shady',
+    name: 'Slim Shady',
+    race: 'Valrath',
+    traits: ['Arcane ', 'Outcast ', 'Persuasive'],
+    image:
+      'https://raw.githubusercontent.com/any2cards/worldhaven/refs/heads/master/images/art/frosthaven/characters/images/fh-deathwalker.png',
+    color: 'from-zinc-800/20',
+    borderColor: 'group-hover:border-zinc-500/50',
+  },
+];
